38ea2be6cbe1e847ee0be9b7a9c63d9451e20a28
Lucy <itslucyday@gmail.com>
2025-03-05 12:41:38 -0500

    adding and subtracting

--- controller.py
+++ controller.py
@@ -2,24 +2,47 @@ from tkinter import *
 from tkinter import ttk
 
 root = Tk()
+root.title('Electric Car Controller')
 
-def layout(self):
-    ttk.Label(self, text='test').grid(column=3, row=1, sticky=N)
+total = StringVar()
+total.set('0')
+def sub():
+    try:
+        if int(total.get()) <= 0:
+            pass
+        else:
+            value = int(total.get())
+            total.set(value = value - 1)
+
+    except ValueError:
+        pass
+def add():
+    try:
+        value = int(total.get())
+        total.set(value = value + 1)
+
+    except ValueError:
+        pass
+
+
+def layout(self, title, total):
+    ttk.Label(self, text=title).grid(column=3, row=1)
+    ttk.Button(self, text='Decrease', command=sub).grid(column=1, row=2)
+    ttk.Button(self, text='Increase', command=add).grid(column=4, row=2)
+    ttk.Label(self, textvariable = total).grid(column=3, row=3)
 
 boundry = ttk.Frame(root)
 mainframe = ttk.Frame(boundry, borderwidth=5, relief='ridge')
 
 boundry.grid(column=0,row=0)
-mainframe.grid(column=0,row=0, columnspan=1, rowspan=2, sticky=(N, W, E, S))
+mainframe.grid(column=0,row=0, columnspan=1, rowspan=2)
 
 topframe = ttk.Frame(mainframe, borderwidth=5, relief='ridge', padding='3 3 12 12')
 bottomframe = ttk.Frame(mainframe, borderwidth=5, relief='ridge', padding='3 3 12 12')
 
-mainframe.columnconfigure(0, weight=1)
-mainframe.rowconfigure(0, weight=1)
 topframe.grid(column=1, row=1, columnspan=5, rowspan=4)
 
 
-layout(topframe)
+layout(topframe, 'Speed', total)
 
 root.mainloop()
